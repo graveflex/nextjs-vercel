39e6b935a455e0219c7c55578e8497dd8e5a87e4
haseebkhangraveflex <haseeb@graveflex.com>
2023-12-21 16:09:11 +0000

    feat: added theme config

--- packages/theme/src/dark.ts
+++
@@ -1,7 +0,0 @@
-import { theme } from '@refract-ui/sc';
-
-const dark = theme();
-
-export type RefractTheme = typeof dark;
-
-export default dark;
--- packages/theme/src/theme.ts
+++ packages/theme/src/theme.ts
@@ -1,39 +1,39 @@
 import { theme } from '@refract-ui/sc';
 
 const defaultTheme = theme({
-    colors: [
-        primary: {
-            yellow: '#FEF600'
-        },
-        neutral: {
-            gray: '#3A3A3A',
-            white: '#FFFFFF',
-            black: '#000000'
-        }
-    ],
-    typography: {
-        h1: {
-            size: 6,
-            lineHeight: 5.689375
-        },
-        h2: {
-            size: 3.75,
-            lineHeight: 3.8125
-        },
-        h3: {
-            size: 2.8125,
-            lineHeight: 3
-        },
-        bodyBold: {
-            size: 1.5,
-            lineHeight: 1.5625,
-            weight: 700
-        },
-        body: {
-            size: 1.5,
-            lineHeight: 1.5625
-        }
+  colors: {
+    primary: {
+      yellow: '#FEF600'
+    },
+    neutral: {
+      gray: '#3A3A3A',
+      white: '#FFFFFF',
+      black: '#000000'
     }
+  },
+  typography: {
+    h1: {
+      size: 6,
+      lineHeight: 5.689375
+    },
+    h2: {
+      size: 3.75,
+      lineHeight: 3.8125
+    },
+    h3: {
+      size: 2.8125,
+      lineHeight: 3
+    },
+    bodyBold: {
+      size: 1.5,
+      lineHeight: 1.5625,
+      weight: 700
+    },
+    body: {
+      size: 1.5,
+      lineHeight: 1.5625
+    }
+  }
 });
 
 export type RefractTheme = typeof defaultTheme;
