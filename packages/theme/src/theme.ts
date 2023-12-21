f1cacdf1f084b95d231e3fe7cee7ee8ca2ebf143
haseebkhangraveflex <haseeb@graveflex.com>
2023-12-21 17:47:50 +0000

    fix: changed theme structure to utilize refract

--- packages/theme/src/theme.ts
+++ packages/theme/src/theme.ts
@@ -1,39 +1,33 @@
 import { theme } from '@refract-ui/sc';
 
 const defaultTheme = theme({
-  colors: {
-    primary: {
-      yellow: '#FEF600'
-    },
-    neutral: {
-      gray: '#3A3A3A',
-      white: '#FFFFFF',
-      black: '#000000'
-    }
-  },
-  typography: {
-    h1: {
-      size: 6,
+  colors: () => ({
+    yellow: '#FEF600',
+    gray: '#3A3A3A',
+    black: '#000',
+    white: '#FFF'
+  }),
+  themeColors: ({ colors }) => ({
+    primary: colors.yellow
+  }),
+  fontVariants: () => ({
+    heading1: {
+      fontSize: '5.75rem',
       lineHeight: 5.689375
     },
-    h2: {
-      size: 3.75,
-      lineHeight: 3.8125
-    },
-    h3: {
-      size: 2.8125,
+    heading2: {
+      fontSize: '3.75rem',
       lineHeight: 3
     },
-    bodyBold: {
-      size: 1.5,
-      lineHeight: 1.5625,
-      weight: 700
+    heading3: {
+      fontSize: '2.8125rem',
+      lineHeight: 3
     },
     body: {
-      size: 1.5,
+      fontSize: '1.5rem',
       lineHeight: 1.5625
     }
-  }
+  })
 });
 
 export type RefractTheme = typeof defaultTheme;
