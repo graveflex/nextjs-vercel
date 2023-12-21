7b03bb27da60128e3e9ad0eaf11e80ebc30139f6
haseebkhangraveflex <haseeb@graveflex.com>
2023-12-21 20:24:11 +0000

    feat: added layout story props

--- apps/web/src/blocks/Layout/Layout.stories.tsx
+++ apps/web/src/blocks/Layout/Layout.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import type { LayoutType } from '.';
+import { Layout as LayoutType } from '@web/payload/payload-types';
+
 import Layout from '.';
 
 const meta: Meta<LayoutType> = {
@@ -16,5 +17,22 @@ export default meta;
 type Story = StoryObj<LayoutType>;
 
 export const Defaults: Story = {
-  args: {}
+  args: {
+    footerItems: [
+      {
+        title: 'Footer link',
+        url: '/'
+      }
+    ],
+    headerItems: [
+      { title: 'Menu', url: '/', type: 'link' },
+      { title: 'CTA', url: '/', type: 'button' }
+    ],
+    logo: {
+      id: 1,
+      url: 'https://images.unsplash.com/photo-1703081397398-6156621d25ce?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8',
+      createdAt: Date.now.toString(),
+      updatedAt: Date.now.toString()
+    }
+  }
 };
