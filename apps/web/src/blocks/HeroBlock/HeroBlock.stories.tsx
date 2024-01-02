b556c6c46218db225d3d236f5a2d62c755b37023
haseebkhangraveflex <haseeb@graveflex.com>
2024-01-02 11:51:59 -0500

    fix: packagejson and added url to hero block story

--- apps/web/src/blocks/HeroBlock/HeroBlock.stories.tsx
+++ apps/web/src/blocks/HeroBlock/HeroBlock.stories.tsx
@@ -22,7 +22,7 @@ export const Defaults: Story = {
     subHeader: "I'm a subheader",
     cta: "I'm a CTA",
     image: {
-      url: '',
+      url: 'https://images.unsplash.com/photo-1682685797088-283404e24b9d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D',
       id: 1,
       createdAt: '',
       updatedAt: '',
