fc0515fec2ade588f8b77d979548c837b476229e
inadeqtfuturs <alexj.christie@gmail.com>
2024-01-05 16:39:12 -0600

    feat(layout): layout done

--- apps/web/src/globals/Layout/index.tsx
+++ apps/web/src/globals/Layout/index.tsx
@@ -3,51 +3,91 @@
 import type { PropsWithChildren } from 'react';
 import React, { useMemo } from 'react';
 import Link from 'next/link';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import ResponsivePayloadImage from '@web/components/ResponsivePayloadImage';
 import { expandedDoc } from '@web/lib/isExpandedDoc';
 import type {
   Image as ImageT,
   MenuItems,
-  Nav
+  Nav as NavT
 } from '@web/payload/payload-types';
 
 const Header = styled.header`
   padding: 1.625rem 2rem;
   height: 96px;
-  border: red solid 1px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
-const Footer = styled.footer``;
+
+const Footer = styled.footer`
+  padding: 0.75rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 1.75rem;
+`;
 
 const Logo = styled(ResponsivePayloadImage)`
   width: 200px;
+  height: 100%;
 `;
 
-const MenuLink = styled(Link)``;
+const MenuLink = styled(Link)`
+  ${({ theme: { box } }) => box.t('menuLink')};
+  text-decoration: none;
 
-export type LayoutType = PropsWithChildren<Nav>;
+  &:hover,
+  &:active,
+  &:focus {
+    opacity: 0.8;
+  }
+
+  &.button {
+    ${({ theme }) => css`
+      color: ${theme.colors.black};
+      background: ${theme.themeColors.primary};
+      padding: 0.625rem 1.5rem;
+      border-radius: 62px;
+    `};
+  }
+`;
+
+const Copyright = styled.p`
+  opacity: 66%;
+  ${({ theme: { box } }) => box.t('menuLink')};
+`;
 
 function Menu(menuItems: MenuItems | undefined) {
   if (!menuItems?.length) {
     return null;
   }
   return (
-    <nav>
+    <Nav>
       {menuItems?.map(({ label, slug, type, id }) => (
         <MenuLink href={slug} key={id} className={type || 'link'}>
           {label}
         </MenuLink>
       ))}
-    </nav>
+    </Nav>
   );
 }
+
+export type LayoutType = PropsWithChildren<NavT>;
 
 function Layout({ children, header, footer }: LayoutType) {
   const logo = expandedDoc<ImageT>(header?.logo);
   const HeaderMenu = useMemo(() => Menu(header?.main), [header?.main]);
+  const FooterMenu = useMemo(
+    () => Menu(footer?.secondary),
+    [footer?.secondary]
+  );
   return (
     <>
       <Header>
@@ -55,7 +95,10 @@ function Layout({ children, header, footer }: LayoutType) {
         {HeaderMenu}
       </Header>
       <main>{children}</main>
-      <Footer />
+      <Footer>
+        {FooterMenu}
+        {footer?.copyright && <Copyright>{footer.copyright}</Copyright>}
+      </Footer>
     </>
   );
 }
