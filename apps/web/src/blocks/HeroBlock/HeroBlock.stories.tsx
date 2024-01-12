17f3c2859e5b7d08e5ca4a8d5aadcb6767f8c982
inadeqtfuturs <alexj.christie@gmail.com>
2024-01-12 10:31:17 -0600

    fix(HeroBlock): update stories

--- apps/web/src/blocks/HeroBlock/HeroBlock.stories.tsx
+++ apps/web/src/blocks/HeroBlock/HeroBlock.stories.tsx
@@ -18,9 +18,22 @@ type Story = StoryObj<HeroBlockType>;
 export const Defaults: Story = {
   args: {
     title: 'Hero block',
+    subTitle: 'subTitle',
     cta: {
       href: 'https://google.com',
       label: 'Test'
+    },
+    background: {
+      id: 1,
+      alt: 'Myopic Logo',
+      updatedAt: '2024-01-05T20:29:44.749Z',
+      createdAt: '2024-01-05T20:29:44.749Z',
+      url: '/images/Logo.svg',
+      filename: 'Logo.svg',
+      mimeType: 'image/svg+xml',
+      filesize: 7144,
+      width: 193,
+      height: 17
     }
   }
 };
