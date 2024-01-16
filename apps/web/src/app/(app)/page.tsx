f9dd849eb5bfeb48b76ff26d73f9ef59dfce66fb
inadeqtfuturs <alexj.christie@gmail.com>
2024-01-16 11:01:16 -0600

    feat(data): fetch data using rest api rather than local

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
+import type { PaginatedDocs } from 'payload/database';
 
 import BlocksRenderer from '@web/components/BlocksRenderer';
-import fetchPayloadData from '@web/lib/fetchPayloadData';
+import fetchPayloadDataRest from '@web/lib/fetchPayloadDataRest';
+import type { Page } from '@web/payload/payload-types';
 
 export const revalidate = 60;
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const data = await fetchPayloadData(async (client) =>
-    client.find({
-      collection: 'pages',
+  const data = await fetchPayloadDataRest<PaginatedDocs<Page>>({
+    endpoint: '/api/pages',
+    params: {
       where: {
         'pageConfig.slug': {
           equals: '/'
         }
       },
       limit: 1
-    })
-  );
+    }
+  });
 
   // if there's an error fetching data, 404
   if ('error' in data || !data.docs[0]?.blocks) {
---
+++ apps/web/src/lib/fetchPayloadDataRest.ts
@@ -0,0 +1,48 @@
+import qs from 'qs';
+
+import { WEB_URL } from 'settings';
+
+export type FetchPayloadRequest = {
+  endpoint: string;
+  params?: object;
+  next?: {
+    revalidate?: false | 0 | number;
+    tags?: string[];
+  };
+  revalidate?: number;
+  accessToken?: string;
+};
+
+export type FetchPayloadResponse<T> = Promise<T | { error: string }>;
+
+// primarily used to fetch data
+async function fetchPayloadDataRest<T>({
+  endpoint,
+  params,
+  next,
+  accessToken
+}: FetchPayloadRequest): FetchPayloadResponse<T> {
+  const url = `${WEB_URL}${endpoint}${qs.stringify(params, {
+    addQueryPrefix: true
+  })}`;
+
+  try {
+    const res = await fetch(url, {
+      next: { revalidate: 10, ...next },
+      headers: {
+        Authorization: `JWT ${accessToken}`
+      }
+    });
+    const { status } = res;
+    if (status < 400) {
+      const data = await res.json();
+      return data;
+    }
+
+    throw new Error(`Data from ${endpoint} not found`);
+  } catch (err) {
+    return { error: err };
+  }
+}
+
+export default fetchPayloadDataRest;
