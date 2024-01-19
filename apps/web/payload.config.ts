32a8030e3ac21a57c332dbeedc12b40227764623
alex <35149816+inadeqtfuturs@users.noreply.github.com>
2024-01-19 13:41:28 -0600

    Feat/workflow pr deploys (#43)
    * feat(preview): init bash script for preview deploys
    
    * feat(workflow): init pr workflow
    
    * fix(workflow): change workflow type
    
    * fix(workflow): move conditional
    
    * fix(workflow): move conditional II
    
    * fix(workflow): more updates
    
    * fix(workflow): handle head ref;
    
    * fix(workflow): get env vars
    
    * fix(workflow): wip
    
    * fix(workflow): logging
    
    * fix(workflow): update vars
    
    * fix(workflow): update vars ii
    
    * fix(workflow): update vars iii
    
    * fix(workflow): update vars iv
    
    * fix(workflow): update vars v
    
    * fix(workflow): update vars vi
    
    * fix(workflow): update vars vii
    
    * fix(workflow): update vars viii
    
    * fix(workflow): update vars ix
    
    * fix(workflow): update vars x
    
    * fix(workflow): update vars xi
    
    * fix(workflow): test against pr
    
    * fix(workflow): finalize workflows
    
    * fix(secrets): fix env vars
    
    * fix(workflow): update equality
    
    * fix(workflow): update equality. update pull
    
    * fix(workflow): double pull
    
    * fix(workflow): fix pull
    
    * fix(workflow): update node/checkout
    
    * fix(workflow): revert export
    
    * build
    
    * build
    
    * fix(workflow): use correct DB name in deploy
    
    * fix(workflow): update close pr
    
    * fix(workflow): updates to creating db and closing db
    
    * fix(workflow): add postgres
    
    * fix(workflow): update db name
    
    * fix(workflow): apt-get postgres
    
    * build
    
    * fix(workflow): get postgres 15

--- apps/web/payload.config.ts
+++ apps/web/payload.config.ts
@@ -1,27 +1,29 @@
 import { postgresAdapter } from '@payloadcms/db-postgres';
 import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
 import { slateEditor } from '@payloadcms/richtext-slate';
+import dotenv from 'dotenv';
 import path from 'path';
 import { buildConfig } from 'payload/config';
 import { vercelBlobAdapter } from 'vercel-blob-storage';
 
-import {
-  BLOB_ENDPOINT_URL,
-  BLOB_READ_WRITE_TOKEN,
-  BLOB_STORE_ID,
-  POSTGRES_URL,
-  WEB_URL
-} from 'settings';
+import { LOCAL, WEB_URL } from 'settings';
 
 import Images from '@web/collections/Images';
 import Pages from '@web/collections/Pages';
 import Users from '@web/collections/User';
 import Nav from '@web/globals/Layout/Layout.config';
 
+dotenv.config({ path: `${__dirname}/../../.env` });
+
+const POSTGRES_URL =
+  process.env.NODE_ENV === 'production' && LOCAL === 'false'
+    ? `${process.env.POSTGRES_URL}?sslmode=require`
+    : process.env.POSTGRES_URL;
+
 const adapter = vercelBlobAdapter({
-  token: BLOB_READ_WRITE_TOKEN,
-  endpointUrl: BLOB_ENDPOINT_URL,
-  storeId: BLOB_STORE_ID
+  token: process.env.BLOB_READ_WRITE_TOKEN,
+  endpointUrl: process.env.BLOB_ENDPOINT_URL,
+  storeId: process.env.BLOB_STORE_ID
 });
 
 export default buildConfig({
