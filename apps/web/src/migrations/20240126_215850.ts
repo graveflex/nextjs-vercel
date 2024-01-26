87a49570afb852ac33db6c70a59c2cedd20a2001
inadeqtfuturs <alexj.christie@gmail.com>
2024-01-26 16:35:56 -0600

    fix(db): add `using`

--- apps/web/src/migrations/20240126_215850.ts
+++ apps/web/src/migrations/20240126_215850.ts
@@ -10,7 +10,7 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
-ALTER TABLE "pages_blocks_hero_block" ALTER COLUMN "sub_title" SET DATA TYPE jsonb;
+ALTER TABLE "pages_blocks_hero_block" ALTER COLUMN "sub_title" SET DATA TYPE jsonb USING (sub_title::jsonb);
 ALTER TABLE "pages_blocks_hero_block" ADD COLUMN "eyebrow" varchar;
 ALTER TABLE "pages_blocks_hero_block" ADD COLUMN "input_placeholder" varchar;
 ALTER TABLE "pages_blocks_hero_block" ADD COLUMN "input_type" "enum_pages_blocks_hero_block_input_type";`);
