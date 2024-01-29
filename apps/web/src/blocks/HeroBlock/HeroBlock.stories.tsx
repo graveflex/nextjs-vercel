8237a3360e038641add6bc20a18757d8a2eceea0
inadeqtfuturs <alexj.christie@gmail.com>
2024-01-29 13:50:04 -0600

    fix(test): fix test coverage

--- apps/web/src/blocks/HeroBlock/HeroBlock.stories.tsx
+++ apps/web/src/blocks/HeroBlock/HeroBlock.stories.tsx
@@ -17,6 +17,44 @@ type Story = StoryObj<HeroBlockType>;
 
 export const Defaults: Story = {
   args: {
+    blockConfig: {
+      layout: 'imgLeftFull'
+    },
+    title: 'Hero block',
+    subTitle: [
+      {
+        children: [
+          {
+            text: 'Here is some text'
+          }
+        ]
+      }
+    ],
+    eyebrow: 'Eyebrow',
+    cta: {
+      href: 'https://google.com',
+      label: 'Test'
+    },
+    input: {
+      type: 'text'
+    },
+    image: {
+      id: 1,
+      alt: 'Myopic Logo',
+      updatedAt: '2024-01-05T20:29:44.749Z',
+      createdAt: '2024-01-05T20:29:44.749Z',
+      url: '/images/Logo.svg',
+      filename: 'Logo.svg',
+      mimeType: 'image/svg+xml',
+      filesize: 7144,
+      width: 193,
+      height: 17
+    }
+  }
+};
+
+export const MissingProps: Story = {
+  args: {
     title: 'Hero block',
     subTitle: [
       {
--- apps/web/src/blocks/HeroBlock/__tests__/index.test.tsx
+++ apps/web/src/blocks/HeroBlock/__tests__/index.test.tsx
@@ -6,10 +6,14 @@ import projectAnnotations from 'ui/utils/testSetup';
 
 import * as stories from '../HeroBlock.stories';
 
-const { Defaults } = composeStories(stories, projectAnnotations);
+const { Defaults, MissingProps } = composeStories(stories, projectAnnotations);
 
 describe('HeroBlock', () => {
   it('component mounts', () => {
     render(<Defaults />);
+  });
+
+  it('component mounts missing optional props', () => {
+    render(<MissingProps />);
   });
 });
