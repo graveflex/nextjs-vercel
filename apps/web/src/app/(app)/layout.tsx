ee20d5d6e06bf3e787a61cf808880596d95bcb3c
inadeqtfuturs <alexj.christie@gmail.com>
2024-01-31 13:59:57 -0600

    fix(theme): fix ssr theme state

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,38 +1,10 @@
 import type { PropsWithChildren } from 'react';
 import React from 'react';
 
-import ThemeProvider from 'theme/src/ThemeProvider';
-
-import Layout from '@web/globals/Layout';
-import fetchPayloadDataRest from '@web/lib/fetchPayloadDataRest';
-import StyledComponentsRegistry from '@web/lib/StyledComponentRegistry';
-import type { Nav } from '@web/payload/payload-types';
-
-export const metadata = {
-  title: 'Homepage',
-  description: 'Homepage'
-};
-
-export const revalidate = 60;
-
 async function RootLayout({ children }: PropsWithChildren<object>) {
-  const data = await fetchPayloadDataRest<Nav>({
-    endpoint: '/api/payload/globals/nav'
-  });
-
-  if ('error' in data) {
-    return null;
-  }
-
   return (
     <html lang="en">
-      <body>
-        <StyledComponentsRegistry>
-          <ThemeProvider>
-            <Layout {...data}>{children}</Layout>
-          </ThemeProvider>
-        </StyledComponentsRegistry>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
