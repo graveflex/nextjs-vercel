0a32773c6c1d2ece2e937db70cf20c061fa0609c
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2024-02-01 14:37:44 -0600

    feat: use edge runtime

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -2,6 +2,8 @@ import type { PropsWithChildren } from 'react';
 import React from 'react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
+export const runtime = 'edge';
+
 async function RootLayout({ children }: PropsWithChildren<object>) {
   return (
     <html lang="en">
