1a419860bc9b76d8ab4a8fa2ef55400763ac124e
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2024-02-06 22:26:15 -0600

    lint

--- apps/web/src/blocks/HeroRevealBlock/HeroRevealBlock.stories.tsx
+++ apps/web/src/blocks/HeroRevealBlock/HeroRevealBlock.stories.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { faker } from '@faker-js/faker';
 import type { Meta, StoryObj } from '@storybook/react';
 import styled from 'styled-components';
+
 import bgImgUrl from './img/blue.svg';
 import pdImgUrl from './img/product-detail.png';
-
 import type { HeroRevealBlockType } from '.';
 import HeroRevealBlock from '.';
 
