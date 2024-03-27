86a2fd7de84d59c32f1cf3043c7e6b62e9a9dea8
siobahnschuck <sschuc@saic.edu>
2024-03-27 08:36:16 -0700

    feat(add): init + basic styles

---
+++ packages/ui/components/Quote/Quote.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import type { QuoteType } from '.';
+import Quote from '.';
+
+const meta: Meta<QuoteType> = {
+  title: 'ui/Quote',
+  component: Quote,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+};
+
+export default meta;
+type Story = StoryObj<QuoteType>;
+
+export const Defaults: Story = {
+  args: {
+    copy: 'Lorem ipsum dolor sit amet consectetur the quick brown fox jumps over the lazy dog.'
+  }
+};
+
+export const LargeText: Story = {
+  args: {
+    copy: 'Lorem ipsum dolor sit amet consectetur the quick brown fox jumps over the lazy dog.',
+    isLarge: true
+  }
+};
