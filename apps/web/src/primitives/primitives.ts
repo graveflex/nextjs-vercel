6600b1de6391b060e679a2c3c7c9a14efcce5fcd
gre-tch <vivusmori@gmail.com>
2024-03-27 16:50:08 -0400

    feat: added common primitives

--- apps/web/src/primitives/primitives.ts
+++ apps/web/src/primitives/primitives.ts
@@ -10,21 +10,53 @@ export type PayloadImageT = Partial<Image> & {
   classOverride?: string;
 };
 
-/* /apps/web/src/components/RichText */
-export type PayloadRichTextT = {
-  content: SerializedEditorState;
+export type PayloadNumberT = {
+  number: number;
 };
 
-export type SerializedLexicalNode = {
-  children?: SerializedLexicalNode[];
-  direction?: string;
-  format?: number;
-  indent?: string | number;
-  type: string;
-  version: number;
-  style?: string;
-  mode?: string;
-  text?: string;
-  tag?: string;
-  value?: string | Image;
+export type PayloadTextT = {
+  text: string;
+};
+
+export type PayloadBridgeTextT = {
+  text: string;
+  className?: string;
+};
+
+/* /apps/web/src/components/PayloadIcons */
+export type PayloadIconT = {
+  name: string /* List all public icon names here */;
+  size?: 'sm' | 'md' | 'lg';
+  color?: 'primary' | 'secondary' | 'bg' | 'fg';
+};
+
+/* /apps/web/src/components/PayloadButton */
+export type PayloadButtonT = {
+  color: 'primary' | 'secondary' | 'bg' | 'fg';
+  icon?: Partial<PayloadIconT>;
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'solid' | 'outlined' | 'link';
+};
+
+/* /apps/web/src/components/PayloadLink */
+export type PayloadLinkT = {
+  href?: string;
+  text: Partial<PayloadTextT> | string;
+  style?: Partial<PayloadButtonT>;
+  newTab?: boolean;
+  linkType?: string;
+};
+
+export type PayloadDateTimeT = {
+  date: string;
+  time?: string;
+  format?: string;
+  className?: string;
+};
+
+export type PayloadFileUploadT = {
+  label?: string;
+  accept?: string;
+  multiple?: boolean;
+  className?: string;
 };
