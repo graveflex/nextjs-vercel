e9f58d1914b4a90cd8f93762e23e5d27e5d9d8bf
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2024-04-21 17:36:04 -0500

    deps(payload): update payload v3 generated code

--- apps/web/src/app/(payload)/layout.tsx
+++ apps/web/src/app/(payload)/layout.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import React from 'react'
+import configPromise from '@payload-config'
+import { RootLayout } from '@payloadcms/next/layouts'
 
-export const metadata = {
-  title: 'Payload Admin',
-  description: 'Payload CMS Admin panel'
-};
+import '@payloadcms/next/css'
+import './custom.scss'
 
-export default function PayloadLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+type Args = {
+  children: React.ReactNode
 }
+
+const Layout = ({ children }: Args) => <RootLayout config={configPromise}>{children}</RootLayout>
+
+export default Layout
