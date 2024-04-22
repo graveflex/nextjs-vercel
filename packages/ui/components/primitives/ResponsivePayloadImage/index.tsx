6639e6257260ab85467e327ef97706e0b7a88c0c
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2024-04-21 20:54:58 -0500

    feat(ResponsiveImage): actually make responsive

--- packages/ui/components/primitives/ResponsivePayloadImage/index.tsx
+++ packages/ui/components/primitives/ResponsivePayloadImage/index.tsx
@@ -1,21 +1,17 @@
 'use client';
 
-import React from 'react';
-import Image from 'next/image';
+import React, { useMemo } from 'react';
+import { getImageProps } from 'next/image';
 import { type Image as PayloadImageProps } from '@mono/types/payload-types';
-import genClassName from '@mono/ui/utils/genClassname';
-import styled from '@refract-ui/sc';
+import styled, { css } from '@refract-ui/sc';
+import { type DefaultTheme, useTheme } from 'styled-components';
 
-export interface ResponsivePayloadWrapperProps {
+const Container = styled.picture``;
+
+export interface ResponsivePayloadWrapperProps
+  extends React.ComponentProps<'img'> {
   image?: number | PayloadImageProps | null | undefined;
-  className?: string;
-  classOverride?: string;
 }
-
-const ImageWrapper = styled.div`
-  container-type: inline-size;
-  position: relative;
-`;
 
 type Dimensions = {
   height?: number;
@@ -30,16 +26,132 @@ const isFill = ({ width, height }: Dimensions): boolean => {
   return false;
 };
 
-function ResponsivePayloadImage({
-  image,
-  className,
-  classOverride
-}: ResponsivePayloadWrapperProps) {
-  if (typeof image === 'number') {
+type Breakpoints = {
+  [K in DefaultTheme['settings']['breakpointNames'][number]]: number;
+};
+
+interface SourceProps {
+  image?: {
+    url?: string | null;
+    width?: number | null;
+    height?: number | null;
+  };
+  breakpoints: Breakpoints;
+  bp: keyof Breakpoints;
+}
+
+function Source({ image, breakpoints, bp }: SourceProps) {
+  if (!image?.url) {
     return null;
   }
 
-  if (!image) {
+  const minWidth = breakpoints[bp];
+
+  return (
+    <source
+      media={`(min-width: ${minWidth}px)`}
+      srcSet={
+        getImageProps({
+          alt: '',
+          width: image.width || undefined,
+          height: image.height || undefined,
+          src: image.url
+        }).props.srcSet
+      }
+    />
+  );
+}
+
+interface BuildMediaQueriesProps {
+  $additionalProps: PayloadImageProps['additionalProps'];
+  $assets: {
+    image?: PayloadImageProps;
+    bp: keyof Breakpoints;
+  }[];
+}
+
+function breakpointStyle(
+  additionalProps: PayloadImageProps['additionalProps'],
+  asset: { image?: PayloadImageProps; bp: keyof Breakpoints }
+) {
+  return css`
+    ${({ theme: { mq } }) => {
+      return mq[asset.bp]`
+        ${
+          additionalProps?.objectFit &&
+          css`
+            object-fit: ${additionalProps?.objectFit};
+          `
+        }
+
+        ${
+          additionalProps?.isRounded
+            ? css`
+                border-radius: 36px;
+              `
+            : css`
+                border-radius: none;
+              `
+        }
+
+        ${
+          additionalProps?.aspectRatio &&
+          css`
+            aspect-ratio: ${additionalProps?.aspectRatio};
+          `
+        }
+      `;
+    }}
+  `;
+}
+
+const buildMediaQueries = ({
+  $additionalProps,
+  $assets
+}: BuildMediaQueriesProps) => {
+  return css`
+    ${$assets
+      .filter((a) => a?.image?.url)
+      .reduce<ReturnType<typeof css>[]>((coll, asset) => {
+        coll.push(
+          breakpointStyle(
+            {
+              ...$additionalProps,
+              ...(asset?.image?.additionalProps ?? {})
+            },
+            asset
+          )
+        );
+        return coll;
+      }, [])}
+  `;
+};
+
+const ResponsiveImage = styled.img<BuildMediaQueriesProps>`
+  ${({ $assets, $additionalProps }) =>
+    buildMediaQueries({ $additionalProps, $assets })}
+`;
+
+function ResponsivePayloadImage({
+  image,
+  className,
+  ...props
+}: ResponsivePayloadWrapperProps) {
+  const theme = useTheme();
+
+  const breakpoints = useMemo(
+    () =>
+      theme.settings.breakpointNames.reduce<Partial<Breakpoints>>(
+        (coll, name, idx) => ({
+          ...coll,
+          [name]: theme.settings.breakpointValues[idx]
+        }),
+        {}
+      ) as Breakpoints,
+    [theme.settings.breakpointValues, theme.settings.breakpointNames]
+  );
+
+  if (typeof image === 'number' || !image) {
     return null;
   }
 
@@ -49,17 +161,22 @@ function ResponsivePayloadImage({
     return null;
   }
 
+  const { mobile, tablet, desktop, ultrawide } = sizes ?? {};
+  let { thumbnail } = sizes ?? {};
+
+  if (!thumbnail) {
+    thumbnail = { url, width, height };
+  }
+
   /* Nextjs Image properties. There cannot be a height and width if fill is true */
   const fill = imageProps?.fill ?? isFill({ height, width } as Dimensions);
 
-  const { thumbnail, mobile, tablet, desktop, ultrawide } = sizes ?? {};
-
   const fillSizes = [
-    `(max-width: 480px) ${thumbnail?.width}px`,
-    `(max-width: 768px) ${mobile?.width}px`,
-    `(max-width: 1024px) ${tablet?.width}px`,
-    `(max-width: 2048px) ${desktop?.width}px`,
-    `${ultrawide?.width}px`
+    `(min-width: 2048px) ${desktop?.width} px`,
+    `(min-width: 1024px) ${tablet?.width} px`,
+    `(min-width: 768px) ${mobile?.width} px`,
+    `(min-width: 0) ${thumbnail?.width} px`,
+    `${ultrawide?.width} px`
   ].join(', ');
 
   const dimensions = !fill
@@ -69,46 +186,40 @@ function ResponsivePayloadImage({
       } as Dimensions)
     : {};
 
-  /* Additional styling props for the image & container */
-  const objectFit = additionalProps?.objectFit ?? 'cover';
-  const isRounded = additionalProps?.isRounded ?? false;
-  const aspectRatio = additionalProps?.aspectRatio ?? 'initial';
-
-  const imageStyles = {
-    objectFit,
-    borderRadius: isRounded ? '36px' : 'initial'
-  } as React.CSSProperties;
-
-  const containerStyles = {
-    aspectRatio
-  } as React.CSSProperties;
-
-  /* We do not want to optimize SVGs */
-  const containsSVG = /\.svg$/;
-  const isSVG = containsSVG.test(url);
-
   return (
-    <ImageWrapper
-      style={containerStyles}
-      className={genClassName([className, classOverride])}
-    >
-      <Image
+    <Container className={className}>
+      <Source breakpoints={breakpoints} image={ultrawide} bp="xxl" />
+      <Source breakpoints={breakpoints} image={desktop} bp="lg" />
+      <Source breakpoints={breakpoints} image={tablet} bp="md" />
+      <Source breakpoints={breakpoints} image={mobile} bp="sm" />
+      <Source breakpoints={breakpoints} image={thumbnail} bp="xxs" />
+      <ResponsiveImage
+        $additionalProps={additionalProps}
+        $assets={[
+          { image: thumbnail as PayloadImageProps, bp: 'xxs' },
+          { image: mobile as PayloadImageProps, bp: 'sm' },
+          { image: tablet as PayloadImageProps, bp: 'md' },
+          { image: desktop as PayloadImageProps, bp: 'lg' },
+          { image: ultrawide as PayloadImageProps, bp: 'xxl' }
+        ]}
         {...{
           ...dimensions,
-          ...imageProps
+          ...props
         }}
-        quality={imageProps?.quality ?? 75}
-        priority={!!imageProps?.priority}
-        fill={!!fill}
         src={url}
+        srcSet={
+          getImageProps({
+            alt: '',
+            width: width || undefined,
+            height: height || undefined,
+            src: url,
+            fill: !width && !!imageProps?.fill
+          }).props.srcSet
+        }
         sizes={fill ? fillSizes : '100vw'}
         alt={alt ?? ''}
-        unoptimized={isSVG}
-        style={imageStyles}
-        placeholder="blur"
-        blurDataURL={url}
       />
-    </ImageWrapper>
+    </Container>
   );
 }
 
