ded38ffa9a27536d76cb227927d56e193e37b40e
siobahnschuck <sschuc@saic.edu>
2024-06-11 08:09:37 -0700

    fix(types): web test

--- apps/web/src/globals/Layout/__tests__/index.test.tsx
+++ apps/web/src/globals/Layout/__tests__/index.test.tsx
@@ -5,14 +5,10 @@ import { render } from '@testing-library/react';
 
 import * as stories from '../Layout.stories';
 
-const { Defaults, MissingMenu } = composeStories(stories, projectAnnotations);
+const { Defaults } = composeStories(stories, projectAnnotations);
 
 describe('Layout', () => {
   it('component mounts', () => {
     render(<Defaults />);
-  });
-
-  it('component mounts without menus', () => {
-    render(<MissingMenu />);
   });
 });
