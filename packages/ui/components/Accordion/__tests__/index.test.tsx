036c3c12bca05a12b2a2efefb8402c9541ce43d5
queenOfPentacls <90799803+queenOfPentacls@users.noreply.github.com>
2024-06-14 10:20:27 -0500

    feat(Base Components) Accordion Component. DCH-29 (#108)
    * feat: accordion
    
    * Revert "Fix/live preview locales [DCH-12] (#105)"
    
    This reverts commit 34c772c301a62694d6ee6af2596d7043d2a22b43.
    
    * Fix/live preview locales [DCH-12] (#105)
    
    * deps(upgrade): payload 19 -> 36
    
    * feat(add): vercel_domain_url for deployed links live preview
    
    * feat(page): increase depth
    
    * deps(upgrade): next->15, react->19
    
    * feat(add): locale workaround for useLivePreview
    
    * fix(ci): types
    
    * feat: accordion
    
    * feat: revisions
    
    ---------
    
    Co-authored-by: Siobahn <32271860+siobahnschuck@users.noreply.github.com>

---
+++ packages/ui/components/Accordion/__tests__/index.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import projectAnnotations from '@mono/ui/utils/testSetup';
+import { composeStories } from '@storybook/react';
+import { render } from '@testing-library/react';
+
+import * as stories from '../Accordion.stories';
+
+const { Defaults } = composeStories(stories, projectAnnotations);
+
+describe('Accordion', () => {
+  it('component mounts', () => {
+    render(<Defaults />);
+  });
+});
---
+++ packages/ui/components/Accordion/index.tsx
@@ -0,0 +1,182 @@
+'use client';
+
+import React, { useCallback, useMemo, useState } from 'react';
+import RichText from '@mono/ui/components/primitives/RichText';
+import Icon from '@mono/ui/components/RenderIcon';
+import s, { css } from '@refract-ui/sc';
+
+const ContentStyles = s.div`
+  padding-bottom: 1.125rem;
+`;
+
+const Content = s.div`
+  overflow: hidden;
+  height: 0;
+  display: none;
+
+  &.open {
+    height: auto;
+    display: block;
+  }
+`;
+
+const IconContainer = s.div`
+  display: flex;
+  align-items: center;
+  column-gap: 0.75rem;
+`;
+
+const Text = s.span`
+  display: block;
+
+  ${({ theme: { box, allColors } }) => css`
+    ${box.t('h3')};
+    color: ${allColors.plainFg};
+  `}
+`;
+
+const hoverStyles = css`
+  ${({ theme: { allColors } }) => css`
+    &:hover {
+      outline: none;
+
+      svg {
+        color: ${allColors.plainHover};
+      }
+
+      ${Text} {
+        color: ${allColors.plainHover};
+        text-decoration: underline;
+      }
+    }
+  `}
+`;
+
+const TextButton = s.button`
+  all: unset;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  column-gap: 2rem;
+  width: 100%;
+  padding: 1.125rem 0;
+
+  &:hover {
+    background: transparent;
+    box-shadow: none;
+  }
+
+  ${hoverStyles};
+`;
+
+const InnerList = s.li`
+  list-style: none;
+  margin: 0;
+  cursor: pointer;
+  overflow: hidden;
+
+  ${({ theme: { allColors } }) => css`
+    border-top: 1px solid ${allColors.plainFg};
+
+    svg {
+      color: ${allColors.plainFg};
+    }
+
+    ${hoverStyles};
+  `}
+`;
+
+const List = s.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+// Refactor this to instead use a payload type after the FAQBlock is updated
+
+export type AccordionProps = {
+  title?: string;
+  items?: {
+    text?: string | null;
+    content?: {
+      root: {
+        type: string;
+        children: {
+          type: string;
+          version: number;
+          [k: string]: unknown;
+        }[];
+        direction: 'ltr' | 'rtl' | null;
+        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+        indent: number;
+        version: number;
+      };
+      [k: string]: unknown;
+    } | null;
+    id?: string | null;
+  }[];
+  children?: React.ReactNode;
+};
+
+function Accordion({
+  items,
+  children
+}: AccordionProps & { children?: React.ReactNode }) {
+  const [openSections, setOpenSections] = useState<boolean[]>(
+    Array(items?.length).fill(false)
+  );
+
+  const toggleOpen = useCallback(
+    (index: number) => {
+      const updatedOpenSections = openSections.map((item, i) =>
+        i === index ? !item : item
+      );
+      setOpenSections(updatedOpenSections);
+    },
+    [openSections]
+  );
+
+  const accordionItems = useMemo(() => {
+    return items?.map(({ text, content }, index) => {
+      const key = `${text}-${index}`;
+
+      return (
+        <InnerList key={key}>
+          <TextButton
+            aria-label={text ? `Toggle ${text}` : 'Toggle section'}
+            tab-index="0"
+            onClick={() => toggleOpen(index)}
+            aria-expanded={openSections[index]}
+            aria-controls={`section-${index}`}
+          >
+            <Text className={openSections[index] ? 'open' : ''}>{text}</Text>
+            <IconContainer>
+              {openSections[index] ? (
+                <Icon name="MinusSign" size="35" color="currentColor" />
+              ) : (
+                <Icon name="PlusSign" size="35" color="currentColor" />
+              )}
+            </IconContainer>
+          </TextButton>
+          <Content
+            id={`section-${index}`}
+            className={openSections[index] ? 'open' : ''}
+          >
+            <ContentStyles>
+              {content && <RichText {...content} />}
+              {children}
+            </ContentStyles>
+          </Content>
+        </InnerList>
+      );
+    });
+  }, [items, toggleOpen, openSections, children]);
+
+  if (!items) {
+    return null;
+  }
+
+  return <List>{accordionItems}</List>;
+}
+
+export default Accordion;
