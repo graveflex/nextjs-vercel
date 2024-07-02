11eb7d7b4a4b23c4b7fbbe27ed002688e468dbb2
siobahnschuck <sschuc@saic.edu>
2024-07-02 10:36:56 -0700

    feat(move): seed schema to colocated block

---
+++ apps/web/src/blocks/CardGridBlock/CardGridBlockSeed.ts
@@ -0,0 +1,8 @@
+export const cardGridBlock = {
+  id: '6669d7bd6d58e03f8e7c107a',
+  blockName: 'Card Grid Block',
+  blockType: 'cardGridBlock',
+  blockConfig: {
+    contentWidth: 'xxl'
+  }
+};
---
+++ apps/web/src/blocks/FAQBlock/FAQBlockSeed.ts
@@ -0,0 +1,55 @@
+import { faker } from '@faker-js/faker';
+import genRichText from '@mono/ui/utils/genRichText';
+
+export const faqBlockSchema = {
+  id: '6669d7bd6d58e03f8e7c107b',
+  blockName: 'FAQ Block',
+  blockType: 'faqBlock',
+  blockConfig: {
+    contentWidth: 'xl'
+  },
+  header: {
+    ...genRichText([
+      {
+        type: 'heading',
+        tag: 'h2',
+        text: 'Frequently Asked Questions'
+      }
+    ])
+  },
+  items: [
+    {
+      title: faker.lorem.sentence(),
+      content: {
+        ...genRichText([
+          {
+            type: 'paragraph',
+            text: faker.lorem.paragraph()
+          }
+        ])
+      }
+    },
+    {
+      title: faker.lorem.sentence(),
+      content: {
+        ...genRichText([
+          {
+            type: 'paragraph',
+            text: faker.lorem.paragraph()
+          }
+        ])
+      }
+    },
+    {
+      title: faker.lorem.sentence(),
+      content: {
+        ...genRichText([
+          {
+            type: 'paragraph',
+            text: faker.lorem.paragraph()
+          }
+        ])
+      }
+    }
+  ]
+};
