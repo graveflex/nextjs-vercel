6486e5213d1dd21993eec8740327090473c53a1a
siobahnschuck <sschuc@saic.edu>
2024-08-22 13:49:42 -0700

    feat(add): image optimization formats, size, lazy

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -76,6 +76,9 @@ export default withPayload(
       return config;
     },
     images: {
+      formats: ['image/avif', 'image/webp'],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256],
       remotePatterns: [
         {
           protocol: 'https',
