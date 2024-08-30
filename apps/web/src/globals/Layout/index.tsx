e54ea4889a814c97e14894328faead79ac6bb3d8
siobahnschuck <sschuc@saic.edu>
2024-08-30 10:24:39 -0700

    feat(add): lock body scroll + style tweaks

--- apps/web/src/globals/Layout/index.tsx
+++ apps/web/src/globals/Layout/index.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import React, { useState } from 'react';
 import { containerStyles } from '@mono/theme/src/ThemeProvider';
 import type * as themeList from '@mono/theme/src/theme';
 import type { Nav as NavT } from '@mono/types/payload-types';
+import useLockBodyScroll from '@mono/ui/lib/hooks/useLockBodyScroll';
 import Footer from '@mono/ui/components/Footer';
 import Header from '@mono/ui/components/Header';
 import MaybeThemed from '@mono/ui/components/MaybeThemed';
@@ -11,10 +15,12 @@ export interface LayoutType extends PropsWithChildren<NavT> {
 }
 
 function Layout({ children, header, footer, theme }: LayoutType) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  useLockBodyScroll(menuOpen);
   return (
     <MaybeThemed theme={theme} style={containerStyles}>
       <div style={containerStyles}>
-        <Header {...header} />
+        <Header {...header} open={menuOpen} setOpen={setMenuOpen} />
         <main role="main" style={{ zIndex: 0 }}>
           {children}
         </main>
--- packages/ui/components/Header/index.tsx
+++ packages/ui/components/Header/index.tsx
@@ -1,22 +1,21 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import type {
-  BannerContent,
-  CTAType,
   CollapsibleMenu,
+  CTAType,
   FlatMenu,
   IconNavItems,
   Image
 } from '@mono/types/payload-types';
 import CtaButton from '@mono/ui/components/CtaButton';
 import CtaLink from '@mono/ui/components/CtaLink';
-import RenderIcon from '@mono/ui/components/RenderIcon';
 import Link from '@mono/ui/components/primitives/PayloadLink';
 import ResponsivePayloadImage from '@mono/ui/components/primitives/ResponsivePayloadImage';
-import RichText from '@mono/ui/components/primitives/RichText';
+import RenderIcon from '@mono/ui/components/RenderIcon';
 import styled, { css } from '@refract-ui/sc';
 import { motion } from 'framer-motion';
-import React, { useState } from 'react';
+import s from 'styled-components';
 
 // UPDATE TO USE THEMECOLOR
 const OuterHeader = styled.header`
@@ -24,46 +23,87 @@ const OuterHeader = styled.header`
   top: 0;
   z-index: 50;
   ${({ theme: { allColors } }) => css`
-    color: ${allColors.fg};
+    color: ${allColors.dark};
     background-color: ${allColors.bg};
   `}
 `;
 
-const NavContainer = styled.div<{ $open: boolean }>`
-  ${({ theme: { mq }, $open }) => css`
+const NavContainer = styled.div<{ $open: boolean; $scrolled: boolean }>`
+  ${({ theme: { mq }, $open, $scrolled }) => css`
     display: grid;
     justify-content: space-between;
     align-items: center;
     grid-template-areas: 'nav nav';
     overflow-x: hidden;
+    padding: 1rem 1.25rem;
+    border-bottom: ${$scrolled ? '1px solid black' : 'none'};
 
-    ${
-      $open &&
-      css`
+    ${$open &&
+    css`
       grid-template-areas: 'nav nav' 'mobileButtons mobileButtons';
-    `
-    }
+    `}
 
-    ${mq.md`
+    ${mq.lg`
       grid-template-areas: "nav buttons";
-      padding: .5rem 1.875rem;
       gap: 0;
     `}
   `}
 `;
 
-const LinkStyled = styled(Link)`
-  position: relative;
+const LinkStyled = styled.a`
+  ${({ theme: { allColors } }) => css`
+    position: relative;
+    color: ${allColors.fg} !important;
+    letter-spacing: 0.3px;
+    transition: all 0.3s ease;
+    span {
+      transition: 0.3s ease;
+      border-bottom: 1px solid transparent;
+    }
+    text-decoration: none;
+    &:hover {
+      span {
+        border-bottom: 1px solid ${allColors.fg};
+      }
+      color: ${allColors.fg} !important;
+    }
+  `}
+`;
+
+const NextLinkStyled = styled(Link)`
+  ${({ theme: { allColors } }) => css`
+    && {
+      position: relative;
+      color: ${allColors.fg};
+      letter-spacing: 0.3px;
+      transition: all 0.3s ease;
+      span {
+        transition: 0.3s ease;
+        border-bottom: 1px solid transparent;
+      }
+      text-decoration: none;
+      &:hover {
+        span {
+          border-bottom: 1px solid ${allColors.fg};
+        }
+        color: ${allColors.fg};
+      }
+    }
+  `}
 `;
 
 const Logo = styled(ResponsivePayloadImage)`
   width: 0;
   img {
     object-fit: contain;
+    max-width: 122px;
   }
   grid-area: logo;
-  ${({ theme: { mq } }) => mq.md`
+  ${({ theme: { mq } }) => mq.lg`
     display: block;
+    img {
+      max-width: unset;
+    }
   `}
 `;
 
@@ -71,14 +111,14 @@ const Row = styled.div`
   display: flex;
   align-items: center;
   gap: 1rem;
-  align-self: end;
+  align-self: center;
   justify-self: end;
 `;
 
 // UPDATE TO USE THEMECOLOR
 const DesktopRow = styled.div`
   display: none;
-  ${({ theme: { mq } }) => mq.md`
+  ${({ theme: { mq } }) => mq.lg`
     grid-area: buttons;
     display: flex;
   `}
@@ -96,40 +136,59 @@ const DrawerButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  transition: 0.3s ease;
 
   &:hover {
     cursor: pointer;
-    background-color: transparent;
-    box-shadow: none;
+    background-color: transparent !important;
+    box-shadow: none !important;
   }
 
   &:focus {
     outline: none;
+    background-color: transparent !important;
+    box-shadow: none !important;
   }
+`;
 
-  ${({ theme: { mq } }) => mq.md`
+const DrawerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  ${({ theme: { mq } }) => mq.lg`
     display: none;
   `}
 `;
 
-const MobileColumn = styled(motion.div)`
+const CtaWrapper = styled.div`
+  display: none;
+  margin: 0 1rem;
+  ${({ theme: { mq } }) => mq.lg`
+    display: flex;
+  `}
+`;
+
+const MobileColumn = s(motion.div)`
   ${({ theme: { mq, allColors } }) => css`
     position: absolute;
-    top: 6rem;
-    padding-top: 3rem;
+    top: 5.25rem;
+    padding-top: 2rem;
     width: 100%;
-    height: 100svh;
+    //Height of header subtracted from height of page
+    height: calc(100svh - 84px);
     overflow-y: auto;
     background-color: ${allColors.bg};
 
-    ${mq.md`
-      display: none;
+    ${mq.lg`
+      // Need to use '!important' here to get specificity over inline styles loaded through 'variants' 
+      display: none !important;
     `}
   `}
 `;
 
 const Nav = styled.nav`
-  padding: 0.5rem 1.5rem;
+  margin: auto;
+  width: 100%;
   display: grid;
   grid-area: nav;
   align-items: center;
@@ -139,86 +198,105 @@ const Nav = styled.nav`
 const NavContentWrapper = styled.div`
   ${({ theme: { mq } }) => css`
     width: 100%;
+    height: 95%;
     display: flex;
     flex-direction: column;
     align-items: start;
-    padding: 0 2rem;
-    gap: 2rem;
+    padding: 0 1.25rem;
 
-    ${mq.md`
+    ${mq.lg`
       flex-direction: row;
       align-items: center;
-      gap: 1rem;
+      gap: 1.5rem;
     `}
   `}
 `;
 
-const Banner = styled.div<{ $bgC: string }>`
-  background-color: ${({ $bgC }) => $bgC};
-  color: white;
-  padding: 0.5rem 1.5rem;
-  max-height: max-content;
-  text-align: center;
-`;
-
 const DesktopIconLink = styled.a`
   display: none;
-  ${({ theme: { mq } }) => mq.md`
+  ${({ theme: { mq } }) => mq.lg`
     display: flex;
   `}
 `;
 
 const MobileIconLink = styled.a`
   display: flex;
-  ${({ theme: { mq } }) => mq.md`
+  ${({ theme: { mq } }) => mq.lg`
     display: none;
   `}
 `;
 
 const ItemWrapper = styled.div`
-  display: flex;
-  gap: 0.5rem;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  ${({ theme: { mq } }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    ${mq.lg`
+      gap: 0.5rem;
+    `}
+  `}
 `;
 
 const NavDropdownWrapper = styled.div`
   display: none;
-
-  ${({ theme: { mq } }) => mq.md`
-    display: none;
+  ${({ theme: { mq, allColors } }) => mq.lg`
+    display: flex;
     flex-direction: column;
-    gap: 0.25rem;
+    gap: 0.75rem;
     border: 1px solid black;
     border-radius: 0.5rem;
-    background-color: white;
+    background-color: ${allColors.bg};
     padding: 1.5rem 1.63rem;
     align-items: center;
     z-index: 5;
     position: absolute;
-    top: 93px;
-
-    &:hover {
-      display: flex;
-    }
+    top: 4rem;
   `}
 `;
 
-const MobileNavDropdownWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.25rem;
-  width: 100%;
+const MobileNavDropdownWrapper = styled.div<{ $open: boolean }>`
+  ${({ theme: { mq }, $open }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+    width: 100%;
+    transition: 0.3s ease;
 
-  ${({ theme: { mq } }) => mq.md`
-    display: none;
+    opacity: ${$open ? '1' : '0'};
+    visibility: ${$open ? 'visible' : 'hidden'};
+    height: ${$open ? 'auto' : '0'};
+    overflow: hidden;
+
+    ${mq.lg`
+      display: none;
+    `}
   `}
 `;
 
 const ItemLabel = styled.div`
   display: none;
-  ${({ theme: { mq } }) => mq.md`
+  font-weight: 600;
+  transition: 0.3s ease;
+  ${({ theme: { mq, allColors } }) => mq.lg`
+    display: flex;
+    gap: 0.5rem;
+    letter-spacing: 0.3px;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid transparent;
+    &:hover {
+      cursor: pointer;
+      color: ${allColors.fg};
+    }
+  `}
+`;
+
+const LocaleLabel = styled.div`
+  display: none;
+  transition: 0.3s ease;
+  ${({ theme: { mq, allColors } }) => mq.lg`
     display: flex;
     gap: 0.5rem;
     align-items: center;
@@ -226,31 +304,72 @@ const ItemLabel = styled.div`
     border-bottom: 1px solid transparent;
     &:hover {
       cursor: pointer;
-      border-bottom: 1px solid currentColor;
-      opacity: 0.5;
+      color: ${allColors.fg};
     }
   `}
 `;
 
-const MobileItemLabel = styled(ItemLabel)`
-  && {
-    display: grid;
-  }
-  grid-template-columns: 1fr min-content;
-  width: 100%;
-  justify-content: space-around;
-  border-bottom: 1px solid currentColor;
-  padding: 1rem 0;
-  &:hover {
-    cursor: pointer;
-    border-bottom: 1px solid currentColor;
-    opacity: 0.5;
-  }
-  ${({ theme: { mq } }) => mq.md`
+const MobileItemLabel = s(ItemLabel)`
+  ${({ theme: { allColors, box } }) => css`
     && {
-      display: none;
+      display: grid;
+      font-weight: 400;
+      transition: 0.3s ease;
+    }
+    svg {
+      margin-top: -0.25rem;
+    }
+    grid-template-columns: 1fr min-content;
+    width: 100%;
+    justify-content: space-around;
+    border-bottom: 1px solid currentColor;
+    &:hover {
+      cursor: pointer;
+      border-bottom: 1px solid currentColor;
+      color: ${allColors.fg};
+    }
+    ${box.t('h4')};
+    padding-bottom: 2rem;
+    ${({ theme: { mq } }) => mq.lg`
+      && {
+        display: none;
+      }
+    `}
+  `}
+`;
+
+const MenuLink = s(CtaLink)`
+  ${({ theme: { mq, box } }) => css`
+    && {
+      font-weight: 400;
+      ${box.t('h4')};
+      ${mq.lg`
+      ${box.t('menuLink')};
+      letter-spacing: 0.3px;
+    `}
     }
   `}
+`;
+
+const MenuNavLink = s(CtaLink)`
+  ${({ theme: { mq, box } }) => css`
+    && {
+      font-weight: 400;
+      ${box.t('h4')};
+      ${mq.lg`
+      ${box.t('menuLink')};
+      letter-spacing: 0.3px;
+      border-bottom: none;
+      &:hover {
+        border-bottom: none;
+      }
+    `}
+    }
+  `}
+`;
+
+const MenuLinkMobile = s(CtaLink)`
+  font-weight: 600;
 `;
 
 const NavDropdownItem = styled.div<{ $open: boolean }>`
@@ -258,37 +377,67 @@ const NavDropdownItem = styled.div<{ $open: boolean }>`
   flex-direction: column;
   gap: 0.5rem;
   width: 100%;
+  padding: 1rem 0;
+  transition: 0.3s ease;
+  &:last-child {
+    margin-top: auto;
+  }
   ${({ $open }) => css`
-    ${
-      $open &&
-      css`
-      ${MobileItemLabel} {
+    ${$open &&
+    css`
+      .mobileItemLabel {
         display: grid;
-        border-bottom: 1px solid transparent;
+        border-bottom: none;
         svg {
           transform: rotate(180deg);
           transition: transform 0.25s;
         }
       }
 
-      ${MobileNavDropdownWrapper} {
+      .mobileNavDropdownWrapper {
         border-bottom: 1px solid currentColor;
         padding-bottom: 1.5rem;
       }
-    `
-    }
+
+      .mobileLocaleLabel {
+        margin-top: auto;
+        svg {
+          transform: rotate(180deg);
+          transition: transform 0.25s;
+        }
+      }
+    `}
   `}
 
-  ${({ theme: { mq } }) => mq.md`
+  ${({ theme: { mq } }) => mq.lg`
     width: unset;
     height: 100%;
     justify-content: center;
+    &:last-child {
+      margin-top: unset;
+      min-width: 4.875rem;
+    }
+
+    ${NavDropdownWrapper} {
+      visibility: hidden;
+      opacity: 0;
+      transition: 0.3s ease;
+    }
+
     &:hover ${NavDropdownWrapper} {
-      display: flex;
+      visibility: visible;
+      opacity: 1;
     }
 
     &:hover ${ItemLabel} {
       svg {
+        transform: rotate(180deg);
+        transition: transform 0.25s;
+      }
+    }
+
+    &:hover ${LocaleLabel} {
+      & > svg {
         transform: rotate(180deg);
         transition: transform 0.25s;
       }
@@ -307,13 +456,14 @@ type Buttons = {
 
 export type HeaderType = {
   logo?: Image | number | null;
-  banner?: BannerContent | null;
+  open: boolean;
   collapsibleMenu?: CollapsibleMenu | null;
   flatMenu?: FlatMenu | null;
   iconItems?: IconNavItems | null;
   ctaButton?: {
     cta?: CTAType;
   };
+  setOpen: (open: boolean) => void;
 };
 
 function NavContent({
@@ -330,7 +480,6 @@ function NavContent({
       {collapsibleMenu?.sections &&
         collapsibleMenu.sections.map((item) => {
           const itemIsOpen = !!openDropdown && item.id === curr;
-
           return (
             <NavDropdownItem
               key={`collapsible-${item.id}`}
@@ -339,60 +488,58 @@ function NavContent({
               <ItemWrapper>
                 <ItemLabel>
                   <p>{item.label}</p>
-                  <RenderIcon name="CaretDown" color="#FFFFFF" size="20" />
+                  <RenderIcon name="CaretDown" size="20" />
                 </ItemLabel>
                 <MobileItemLabel
+                  className="mobileItemLabel"
                   onClick={() => {
-                    setOpenDropdown(!openDropdown);
-                    setCurr(item.id);
+                    setOpenDropdown(
+                      !openDropdown ? !openDropdown : openDropdown
+                    );
+                    setCurr(item.id === curr ? '' : item.id);
                   }}
                 >
                   <p>{item.label}</p>
-                  <RenderIcon name="CaretDown" color="#FFFFFF" size="20" />
+                  <RenderIcon name="CaretDown" size="30" />
                 </MobileItemLabel>
                 <NavDropdownWrapper className="dropdown-content">
                   {item?.links &&
                     item.links.map((link) => {
                       return (
-                        <CtaLink
-                          $color="bg"
+                        <MenuLink key={`link-${link.id}`} link={link.link} />
+                      );
+                    })}
+                </NavDropdownWrapper>
+                <MobileNavDropdownWrapper
+                  className="mobileNavDropdownWrapper"
+                  $open={!!itemIsOpen}
+                >
+                  {item?.links &&
+                    item.links.map((link) => {
+                      return (
+                        <MenuLinkMobile
                           key={`link-${link.id}`}
                           link={link.link}
                         />
                       );
                     })}
-                </NavDropdownWrapper>
-                {!!itemIsOpen && (
-                  <MobileNavDropdownWrapper>
-                    {item?.links &&
-                      item.links.map((link) => {
-                        return (
-                          <CtaLink
-                            $color="fg"
-                            key={`link-${link.id}`}
-                            link={link.link}
-                          />
-                        );
-                      })}
-                  </MobileNavDropdownWrapper>
-                )}
+                </MobileNavDropdownWrapper>
               </ItemWrapper>
             </NavDropdownItem>
           );
         })}
       {flatMenu &&
         flatMenu.map((item) => {
-          return <CtaLink key={`flatmenu-${item.id}`} link={item?.link} />;
+          return <MenuNavLink key={`flatmenu-${item.id}`} link={item?.link} />;
         })}
-      {ctaButton && ctaButton?.cta && <CtaButton cta={ctaButton?.cta} />}
+      <CtaWrapper>
+        {ctaButton && ctaButton?.cta && <CtaButton cta={ctaButton?.cta} />}
+      </CtaWrapper>
       {iconItems &&
         iconItems.map((item) => {
           return (
             <DesktopIconLink href={item.href as string} key={`icon-${item.id}`}>
-              <RenderIcon
-                {...item.icon}
-                color={item?.icon?.color || '#FFFFFF'}
-              />
+              <RenderIcon {...item.icon} />
             </DesktopIconLink>
           );
         })}
@@ -402,31 +549,39 @@ function NavContent({
 
 function Header({
   logo,
-  banner,
   collapsibleMenu,
   flatMenu,
   iconItems,
-  ctaButton
+  ctaButton,
+  open,
+  setOpen
 }: HeaderType) {
-  const [open, setOpen] = useState(false);
   const openMenuVariants = {
     open: { right: '0%', display: 'block' },
     closed: { right: '-100%', display: 'none' }
   };
 
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <OuterHeader>
-      {banner?.content && (
-        <Banner $bgC={banner.background ?? 'currentColor'}>
-          <RichText {...banner.content} />
-        </Banner>
-      )}
-      <NavContainer $open={open}>
+      <NavContainer $open={open} $scrolled={scrolled}>
         <Nav>
           {logo && (
-            <LinkStyled href="/" title="Home">
+            <NextLinkStyled href="/" prefetch={false} title="Home">
               <Logo image={logo} />
-            </LinkStyled>
+            </NextLinkStyled>
           )}
           <Row>
             {iconItems &&
@@ -434,22 +589,28 @@ function Header({
                 return (
                   <MobileIconLink
                     href={item.href as string}
-                    key={`icon-${item.id}`}
+                    key={`mobileicon-${item.id}`}
                   >
-                    <RenderIcon
-                      {...item.icon}
-                      color={item?.icon?.color || '#FFFFFF'}
-                    />
+                    <RenderIcon {...item.icon} />
                   </MobileIconLink>
                 );
               })}
-            <DrawerButton onClick={() => setOpen(!open)} aria-label="Open Menu">
-              {open ? (
-                <RenderIcon name="Close" size="25" color="#FFFFFF" />
-              ) : (
-                <RenderIcon name="Hamburger" size="25" color="#FFFFFF" />
+
+            <DrawerWrapper>
+              {ctaButton && ctaButton?.cta && (
+                <CtaButton cta={ctaButton?.cta} />
               )}
-            </DrawerButton>
+              <DrawerButton
+                onClick={() => setOpen(!open)}
+                aria-label="Open Menu"
+              >
+                {open ? (
+                  <RenderIcon name="Close" size="25" />
+                ) : (
+                  <RenderIcon name="Hamburger" size="25" />
+                )}
+              </DrawerButton>
+            </DrawerWrapper>
           </Row>
         </Nav>
         <DesktopRow>
