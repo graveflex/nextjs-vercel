c92547d9ecc54717bc3fc4d8a114187762be26ae
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2024-09-02 19:06:42 -0500

    ci: add baseline example with image

--- apps/web/src/app/(app)/refract/page.client.tsx
+++ apps/web/src/app/(app)/refract/page.client.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import styled from '@refract-ui/sc';
+import Image from 'next/image';
 
 const Container = styled.div`
   border-radius: 4px;
@@ -18,14 +19,30 @@ const P = styled.p`
   color: #666;
 `;
 
+const Img = styled(Image)`
+  width: 100%;
+  height: auto;
+  aspect-ratio: 4/3;
+  object-fit: cover;
+  background: #666;
+`;
+
 interface StyledPageProps {
   heading: string;
   body?: string;
+  imageSrc?: string;
 }
 
-export default function StyledPage({ heading, body }: StyledPageProps) {
+export default function StyledPage({ heading, body, imageSrc }: StyledPageProps) {
   return (
     <Container>
+      {imageSrc && (<Img
+        loading="eager"
+        width={500}
+        height={500}
+        alt="Demo image"
+        src={imageSrc}
+      />)}
       <H2>{heading}</H2>
       {body && <P>{body}</P>}
     </Container>
