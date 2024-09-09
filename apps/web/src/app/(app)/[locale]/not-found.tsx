a6ba1c6d8aa594c55aff451ce6ac639b10066b52
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2024-09-09 13:33:50 -0500

    wip(404): inherit nav from layout.tsx

--- apps/web/src/app/(app)/[locale]/not-found.tsx
+++ apps/web/src/app/(app)/[locale]/not-found.tsx
@@ -1,32 +1,9 @@
 import FourOhFour from '@mono/web/globals/FourOhFour/FourOhFour.client';
-import { DEFAULT_LOCALE, type LOCALES } from '@mono/web/lib/constants';
-import config from '@payload-config';
-import { getPayloadHMR } from '@payloadcms/next/utilities';
-import { cookies } from 'next/headers';
 import React from 'react';
 
-export const dynamic = 'auto';
+export const dynamic = 'force-static';
 export const revalidate = 60;
 
 export default async function NotFound() {
-  const payload = await getPayloadHMR({ config });
-  const locale = (cookies().get('NEXT_LOCALE')?.value ||
-    DEFAULT_LOCALE) as (typeof LOCALES)[number];
-
-  try {
-    const defaultNavData = await payload.findGlobal({
-      slug: 'nav',
-      locale,
-      depth: 2,
-      fallbackLocale: DEFAULT_LOCALE
-    });
-
-    if ('error' in defaultNavData) {
-      return { error: 'Error fetching data' };
-    }
-
-    return <FourOhFour nav={defaultNavData} />;
-  } catch (_) {
-    return null;
-  }
+  return <FourOhFour />;
 }
--- apps/web/src/globals/FourOhFour/FourOhFour.client.tsx
+++ apps/web/src/globals/FourOhFour/FourOhFour.client.tsx
@@ -1,52 +1,9 @@
-'use client';
-
-import type {
-  FourOhFour as FourOhFourTypes,
-  Nav
-} from '@mono/types/payload-types';
-import LayoutClient from '@mono/web/globals/Layout/Layout.client';
-import { DEFAULT_LOCALE, type LOCALES } from '@mono/web/lib/constants';
-import { WEB_URL } from '@mono/web/lib/constants';
 import { useTranslations } from 'next-intl';
-import { usePathname } from 'next/navigation';
 import type React from 'react';
-import { useEffect, useState } from 'react';
 
-type FourOhFourProps = {
-  nav: Nav;
-};
-
-export default function FourOhFour({ nav }: FourOhFourProps) {
-  const [navData, setNavData] = useState<Nav>(nav ?? null);
-  const path = usePathname();
-  const locale =
-    (path?.split('/')[1] as (typeof LOCALES)[number]) ?? DEFAULT_LOCALE;
-  const webUrl =
-    WEB_URL.includes('undefined') || !WEB_URL ? path?.split('/')[0] : WEB_URL;
+export default function FourOhFour() {
   // Using 404 content from translations rather than the cms
   const t = useTranslations('FourOhFour');
-
-  useEffect(() => {
-    const fetchMarkdownData = async () => {
-      const routeNav = `${webUrl}/api/globals/nav?locale=${locale}`;
-
-      try {
-        const [navResponse] = await Promise.all([
-          fetch(routeNav).then((res) => res.json())
-        ]);
-
-        setNavData(navResponse);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchMarkdownData();
-  }, [locale, webUrl]);
-
-  if (!navData) {
-    return null;
-  }
 
   const styles: React.CSSProperties = {
     alignContent: 'center',
@@ -56,11 +13,9 @@ export default function FourOhFour({ nav }: FourOhFourProps) {
   };
 
   return (
-    <LayoutClient {...navData}>
-      <div style={styles}>
-        <h1>{t('title')}</h1>
-        <p>{t('content')}</p>
-      </div>
-    </LayoutClient>
+    <div style={styles}>
+      <h1>{t('title')}</h1>
+      <p>{t('content')}</p>
+    </div>
   );
 }
