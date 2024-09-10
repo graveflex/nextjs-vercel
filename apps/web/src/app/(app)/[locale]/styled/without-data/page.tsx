6a4cae2f7f93f62cfbeb0a5a5c75f4f666cc20ab
siobahnschuck <sschuc@saic.edu>
2024-09-10 12:05:09 -0700

    hotfix(remove): test lighthouse dirs

--- apps/web/src/app/(app)/[locale]/styled/without-data/page.tsx
+++
@@ -1,7 +0,0 @@
-import PageClient from '../page.client';
-
-export default function BaselineWithoutDataPage() {
-  return (
-    <PageClient heading="This page contains basic styles and pulls NO data from the API" />
-  );
-}
