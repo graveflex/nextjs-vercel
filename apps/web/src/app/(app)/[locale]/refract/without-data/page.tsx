412b52cc9918d174de2da6fce850c2dc9b992907
siobahnschuck <sschuc@saic.edu>
2024-09-10 12:05:09 -0700

    hotfix(remove): test lighthouse dirs

--- apps/web/src/app/(app)/[locale]/refract/without-data/page.tsx
+++
@@ -1,7 +0,0 @@
-import PageClient from '../page.client';
-
-export default async function RefractWithoutDataPage() {
-  return (
-    <PageClient heading="This page contains basic refract styles and pulls NO data from the API" />
-  );
-}
