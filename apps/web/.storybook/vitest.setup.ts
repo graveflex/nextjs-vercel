27d7c7842d6b49247415e51882a41ac213781eea
Brett Cimbalik <bcimbali@gmail.com>
2025-01-22 11:35:56 -0500

    wip(tests): initial storybook manual exp testSetup

---
+++ apps/web/.storybook/vitest.setup.ts
@@ -0,0 +1,10 @@
+import { beforeAll } from 'vitest';
+// 👇 If you're using Next.js, import from @storybook/nextjs
+//   If you're using Next.js with Vite, import from @storybook/experimental-nextjs-vite
+import { setProjectAnnotations } from '@storybook/react';
+import * as previewAnnotations from './preview';
+
+const annotations = setProjectAnnotations([previewAnnotations]);
+
+// Run Storybook's beforeAll hook
+beforeAll(annotations.beforeAll);
