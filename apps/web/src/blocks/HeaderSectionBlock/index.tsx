16bb3ebde3e50a3ec9e86590e7d140918de6281b
Brett Cimbalik <bcimbali@gmail.com>
2025-01-27 15:29:45 -0500

    feat(block): add dynamic layouts to HeaderSection

--- apps/web/src/blocks/HeaderSectionBlock/index.tsx
+++ apps/web/src/blocks/HeaderSectionBlock/index.tsx
@@ -1,39 +1,29 @@
 import type { HeaderSectionBlockT as PayloadType } from '@mono/types/payload-types';
 import Wrapper from '@mono/ui/components/Wrapper';
+import dynamic from 'next/dynamic';
 import React from 'react';
+import type { ComponentType } from 'react';
 
 export type HeaderSectionBlockType = Omit<PayloadType, 'blockType'>;
 
+const defaultOpts = {
+  suspense: true,
+  ssr: true
+};
+
+// Todo: Convert into a prop:
+const layout: number = 1;
+
+const layoutList: Record<number, ComponentType<HeaderSectionBlockType>> = {
+  1: dynamic(() => import('./layouts/HeaderSection1'), {
+    ...defaultOpts
+  })
+};
+
 function HeaderSectionBlock({ title }: HeaderSectionBlockType) {
-  return (
-    <section
-      className="bg-background py-16 lg:py-24"
-      aria-labelledby="page-heading"
-    >
-      <div className="container px-6 mx-auto">
-        <div className="flex flex-col gap-6 lg:gap-8 flex-1 max-w-xl items-center text-center mx-auto">
-          <div className="flex flex-col gap-4 lg:gap-5">
-            <p
-              className="text-muted-foreground text-sm lg:text-base font-semibold"
-              aria-hidden="true"
-            >
-              {title}
-            </p>
-            <h1
-              id="page-heading"
-              className="text-foreground text-3xl md:text-5xl font-bold"
-            >
-              Short engaging headline
-            </h1>
-            <p className="text-muted-foreground text-base lg:text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit interdum
-              hendrerit ex vitae sodales.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  const Component: ComponentType<HeaderSectionBlockType> = layoutList[layout];
+
+  return <Component title={title} />;
 }
 
 export default HeaderSectionBlock;
