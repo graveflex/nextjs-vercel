4aac1b50e401464a610dac6df5f9a5de15457d21
Brett Cimbalik <bcimbali@gmail.com>
2025-01-27 17:10:42 -0500

    feat(block): add migration for HeroSectionsBlock

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250120_234732 from './20250120_234732';
 import * as migration_20250121_235819 from './20250121_235819';
+import * as migration_20250127_221020 from './20250127_221020';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250121_235819.up,
     down: migration_20250121_235819.down,
-    name: '20250121_235819'
+    name: '20250121_235819',
+  },
+  {
+    up: migration_20250127_221020.up,
+    down: migration_20250127_221020.down,
+    name: '20250127_221020'
   },
 ];
