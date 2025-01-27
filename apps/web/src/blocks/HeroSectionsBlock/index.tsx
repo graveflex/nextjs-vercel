91223d942e38ac4f95cc69949f575f592f5889fa
Brett Cimbalik <bcimbali@gmail.com>
2025-01-27 17:27:55 -0500

    feat(block): add 1st layout to HeroSectionsBlock

--- apps/web/src/blocks/HeroSectionsBlock/index.tsx
+++ apps/web/src/blocks/HeroSectionsBlock/index.tsx
@@ -1,21 +1,29 @@
 import type { HeroSectionsBlockT as PayloadType } from '@mono/types/payload-types';
 import Wrapper from '@mono/ui/components/Wrapper';
+import dynamic from 'next/dynamic';
 import React from 'react';
+import type { ComponentType } from 'react';
 
 export type HeroSectionsBlockType = Omit<PayloadType, 'blockType'>;
 
-function HeroSectionsBlock({ title, blockConfig }: HeroSectionsBlockType) {
-  return (
-    <Wrapper {...blockConfig} hidden={blockConfig?.hidden ?? false}>
-      <div>
-        <h1>HeroSectionsBlock</h1>
-        <div>
-          <h1>title-</h1>
-          <p>{title}</p>
-        </div>
-      </div>
-    </Wrapper>
-  );
+const defaultOpts = {
+  suspense: true,
+  ssr: true
+};
+
+// Todo: Convert into a prop:
+const layout: number = 1;
+
+const layoutList: Record<number, ComponentType<HeroSectionsBlockType>> = {
+  1: dynamic(() => import('./layouts/HeroSections1'), {
+    ...defaultOpts
+  })
+};
+
+function HeroSectionsBlock({ title }: HeroSectionsBlockType) {
+  const Component: ComponentType<HeroSectionsBlockType> = layoutList[layout];
+
+  return <Component title={title} />;
 }
 
 export default HeroSectionsBlock;
