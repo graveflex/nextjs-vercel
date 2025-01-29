08700c7d10b0cf402bc3c4b97d7802abafa6ddda
Brett Cimbalik <bcimbali@gmail.com>
2025-01-29 16:54:41 -0500

    feat(component): add rich-text global styles

--- apps/web/src/components/RichText/index.tsx
+++ apps/web/src/components/RichText/index.tsx
@@ -1,18 +1,9 @@
 import type { CtaSectionsBlockT as PayloadType } from '@mono/types/payload-types';
-import {
-  type DefaultNodeTypes,
-  SerializedBlockNode,
-  SerializedHeadingNode,
-  SerializedLinkNode,
-  SerializedListItemNode,
-  SerializedListNode
-} from '@payloadcms/richtext-lexical';
-import {
-  type JSXConvertersFunction,
-  RichText as LexicalRichText,
-  LinkJSXConverter
-} from '@payloadcms/richtext-lexical/react';
+import { cn } from '@mono/web/lib/utils';
+import { RichText as LexicalRichText } from '@payloadcms/richtext-lexical/react';
 import React from 'react';
+import styles from './RichText.module.css';
+
 export type CtaSectionsBlockType = Omit<PayloadType, 'blockType'>;
 
 export type RichTextType = {
@@ -33,83 +24,16 @@ export type RichTextType = {
   } | null;
 };
 
-const jsxConverters: JSXConvertersFunction<DefaultNodeTypes> = ({
-  defaultConverters
-}) => ({
-  ...defaultConverters,
-  heading: ({ node }) => {
-    switch (node?.tag) {
-      case 'h1':
-        return (
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary-foreground">
-            {node?.children
-              ?.map((child) => ('text' in child ? child.text : ''))
-              .join('')}
-          </h1>
-        );
-      case 'h2':
-        return (
-          <h2 className="text-3xl md:text-4xl font-bold text-primary-foreground">
-            {node?.children
-              ?.map((child) => ('text' in child ? child.text : ''))
-              .join('')}
-          </h2>
-        );
-      case 'h3':
-        return (
-          <h3 className="text-3xl md:text-4xl font-bold text-primary-foreground">
-            {node?.children
-              ?.map((child) => ('text' in child ? child.text : ''))
-              .join('')}
-          </h3>
-        );
-      case 'h4':
-        return (
-          <h4 className="text-3xl md:text-4xl font-bold text-primary-foreground">
-            {node?.children
-              ?.map((child) => ('text' in child ? child.text : ''))
-              .join('')}
-          </h4>
-        );
-      case 'h5':
-        return (
-          <h5 className="text-3xl md:text-4xl font-bold text-primary-foreground">
-            {node?.children
-              ?.map((child) => ('text' in child ? child.text : ''))
-              .join('')}
-          </h5>
-        );
-      case 'h6':
-        return (
-          <h6 className="text-3xl md:text-4xl font-bold text-primary-foreground">
-            {node?.children
-              ?.map((child) => ('text' in child ? child.text : ''))
-              .join('')}
-          </h6>
-        );
-      default:
-        return (
-          <h1 className="text-3xl md:text-4xl font-bold text-primary-foreground">
-            {node?.children
-              ?.map((child) => ('text' in child ? child.text : ''))
-              .join('')}
-          </h1>
-        );
-    }
-  },
-  paragraph: ({ node }) => {
-    return (
-      <p className="text-base md:text-lg leading-7 [&:not(:first-child)]:mt-6 text-primary-foreground">
-        {node?.children
-          ?.map((child) => ('text' in child ? child.text : ''))
-          .join('')}
-      </p>
-    );
-  }
-});
-
 function RichText({ data }: RichTextType) {
-  return data && <LexicalRichText data={data} converters={jsxConverters} />;
+  return (
+    data && (
+      <div
+        className={cn('rich-text-container rich-text-global', styles.richtext)}
+      >
+        <LexicalRichText data={data} className="rich-text" />
+      </div>
+    )
+  );
 }
 
 export default RichText;
