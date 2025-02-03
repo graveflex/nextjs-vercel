1cc86df1fb4c031451e9d00119abca09646ab607
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2025-02-03 13:58:53 -0600

    lint

--- apps/web/src/components/BlocksRenderer/index.tsx
+++ apps/web/src/components/BlocksRenderer/index.tsx
@@ -17,8 +17,8 @@ const blockList = {
 };
 
 function BlocksRenderer({ blocks }: { blocks: NonNullable<Page['blocks']> }) {
-  // TODO: this is only here so the lighthouse check will pass. 
-  // remove once there is header + footer content 
+  // TODO: this is only here so the lighthouse check will pass.
+  // remove once there is header + footer content
   if (!blocks?.length) {
     return <h2>This page is empty</h2>;
   }
@@ -29,7 +29,6 @@ function BlocksRenderer({ blocks }: { blocks: NonNullable<Page['blocks']> }) {
     if (Component) {
       return <Component {...blockProps} key={blockProps.id} />;
     }
-
   });
 }
 
