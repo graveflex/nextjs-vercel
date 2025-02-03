567662401b51ce82b7e428022292330b8e5a86e1
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2025-02-03 15:14:04 -0600

    fix(HeaderSectionBlock): remove unused variants

--- apps/web/src/blocks/HeaderSectionBlock/HeaderSectionBlock.config.ts
+++ apps/web/src/blocks/HeaderSectionBlock/HeaderSectionBlock.config.ts
@@ -38,18 +38,6 @@ const HeaderSectionBlock = (prefix: string): Block => ({
         {
           label: '6',
           value: '6'
-        },
-        {
-          label: '7',
-          value: '7'
-        },
-        {
-          label: '8',
-          value: '8'
-        },
-        {
-          label: '9',
-          value: '9'
         }
       ],
       admin: {
