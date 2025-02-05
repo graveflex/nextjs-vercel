2b00b26028529a4b343b7bed6a3c92cb358f0955
tubstrr <me@jonknoll.dev>
2025-02-05 15:51:06 -0600

    update: actually fix migrations

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,12 +1,6 @@
-import * as migration_20250203_213144 from './20250203_213144';
 import * as migration_20250205_212459 from './20250205_212459';
 
 export const migrations = [
-  {
-    up: migration_20250203_213144.up,
-    down: migration_20250203_213144.down,
-    name: '20250203_213144',
-  },
   {
     up: migration_20250205_212459.up,
     down: migration_20250205_212459.down,
