69e17b3c4282366eb3f844bd7577d3fcaa920279
Brett Cimbalik <bcimbali@gmail.com>
2025-02-05 10:07:44 -0500

    fix(rebase): fix after rebase off feat/shadcn

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250203_213144 from './20250203_213144';
+import * as migration_20250205_150522 from './20250205_150522';
 
 export const migrations = [
   {
     up: migration_20250203_213144.up,
     down: migration_20250203_213144.down,
-    name: '20250203_213144'
+    name: '20250203_213144',
+  },
+  {
+    up: migration_20250205_150522.up,
+    down: migration_20250205_150522.down,
+    name: '20250205_150522'
   },
 ];
