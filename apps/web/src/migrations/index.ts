9196d12b3525f943eda599aceb1356cf5aa36b36
tubstrr <me@jonknoll.dev>
2025-02-07 13:41:18 -0600

    update: no migrations

--- apps/web/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250207_190537 from './20250207_190537';
-
-export const migrations = [
-  {
-    up: migration_20250207_190537.up,
-    down: migration_20250207_190537.down,
-    name: '20250207_190537'
-  },
-];
