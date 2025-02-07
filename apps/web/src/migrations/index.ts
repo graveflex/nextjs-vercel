faeaffc8305e7c5ced02c76b1fb58f42a5eaf3ca
Brett Cimbalik <bcimbali@gmail.com>
2025-02-07 12:44:31 -0500

    fix(rebase): update migration after rebase

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250205_162536 from './20250205_162536';
+import * as migration_20250207_174141 from './20250207_174141';
 
 export const migrations = [
   {
     up: migration_20250205_162536.up,
     down: migration_20250205_162536.down,
-    name: '20250205_162536'
+    name: '20250205_162536',
+  },
+  {
+    up: migration_20250207_174141.up,
+    down: migration_20250207_174141.down,
+    name: '20250207_174141'
   },
 ];
