634b0de0c50888aa3d1cbcefede6fb1243d9020a
tubstrr <me@jonknoll.dev>
2025-02-07 13:39:22 -0600

    testing

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250207_185008 from './20250207_185008';
+import * as migration_20250207_190537 from './20250207_190537';
 
 export const migrations = [
   {
-    up: migration_20250207_185008.up,
-    down: migration_20250207_185008.down,
-    name: '20250207_185008'
+    up: migration_20250207_190537.up,
+    down: migration_20250207_190537.down,
+    name: '20250207_190537'
   },
 ];
