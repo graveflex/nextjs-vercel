43105bbd8dd8a83b7e5e85fa71d767f8d3e56e9c
Brett Cimbalik <bcimbali@gmail.com>
2025-02-11 14:38:24 -0500

    tweak(migration): tweak migrations after rebasing

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,9 +1,21 @@
 import * as migration_20250211_174506 from './20250211_174506';
+import * as migration_20250211_193439 from './20250211_193439';
+import * as migration_20250211_193514 from './20250211_193514';
 
 export const migrations = [
   {
     up: migration_20250211_174506.up,
     down: migration_20250211_174506.down,
-    name: '20250211_174506'
+    name: '20250211_174506',
+  },
+  {
+    up: migration_20250211_193439.up,
+    down: migration_20250211_193439.down,
+    name: '20250211_193439',
+  },
+  {
+    up: migration_20250211_193514.up,
+    down: migration_20250211_193514.down,
+    name: '20250211_193514'
   },
 ];
